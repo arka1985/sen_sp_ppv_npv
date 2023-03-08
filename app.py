7e03a6acf2ca5723abc0bf2c9d3c217ed7ff4aa5
Dr Arkaprabha Sau <33415109+arka1985@users.noreply.github.com>
2023-03-08 07:18:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import random
 sns.set_style('white')
 
 st.title('Calculation of Positive and Negative Predictive Value of Screening Tests from Sensitivity, Specificity and Disease Prevalance')
-st.subheader(':crimson[Monte Carlo Simulation Approach]')
+st.subheader(':red[Monte Carlo Simulation Approach]')
 st.subheader(':blue[Designed and Developed by Dr. Arkaprabha Sau,MBBS, MD (Gold Medalist), PhD (Research Scholar-Artificial Intelligence & Machine Learning]')
 st.header('Please input the range of Sensitivity, Specificity of the Test and Prevalance of the Disease')
 st.success('Sensitivity')
